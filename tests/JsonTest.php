f8d0117abfdc1bb78365b1f06736c3fbdfa408a3
Zac Olden <zacolden@gmail.com>
2018-06-12 21:00:21 +1000

    JSON tests added

---
+++ tests/JsonTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Store\Store;
+
+final class JsonTest extends TestCase
+{
+    public function testJsonOutput()
+    {
+        $store = new Store([
+            'name' => 'James',
+            'fruits' => ['apple', 'banana', 'orange'],
+            'resources' => [
+                'wood' => 1200,
+                'food' => 200,
+                'stone' => 650,
+                'gold' => 750
+            ]
+        ]);
+        
+        $this->assertEquals('{"name":"James"}', $store->take(1)->toJson());
+        
+        $this->assertEquals('["apple","banana","orange"]', $store->fruits->toJson());
+        
+        $this->assertEquals('{"wood":1200,"food":200,"stone":650,"gold":750}', 
+                $store->last()->toJson()
+        );
+    }
+}
