b2af6d950237416be4b37be282c8e849054de454
Zac Olden <zacolden@gmail.com>
2018-06-13 17:48:15 +1000

    Dot syntax access test

--- tests/AccessorTest.php
+++ tests/AccessorTest.php
@@ -62,4 +62,45 @@ final class AccessorTest extends TestCase
         // nested array access via property access
         $this->assertEquals(1200, $store->resources['wood']);
     }
+    
+    public function testDotSyntax()
+    {
+        $store = new Store([
+            'form' => [
+                'register' => [
+                    'enabled' => true,
+                    'username' => [
+                        'min_length' => 2,
+                        'max_length' => 16,
+                        'regex' => '^[a-zA-Z0-9]+$'
+                    ],
+                    'email' => [
+                        'allow invalid' => false,
+                        'allow_idns' => true,
+                    ]
+                ]
+            ]
+        ]);
+        
+        // dot syntax access
+        $this->assertEquals(true, $store->get('form.register.enabled'));
+        
+        // array access after dot syntax
+        $this->assertEquals('^[a-zA-Z0-9]+$', $store->get('form.register.username')['regex']);
+        
+        // property access after dot syntax
+        $this->assertEquals(16, $store->get('form.register.username')->max_length);
+        
+        // dot syntax access after dot syntax
+        $this->assertEquals(false, $store->get('form.register')->get('email.allow_invalid'));
+        
+        // dot syntax access after array access
+        $this->assertEquals(true, $store['form']['register']->get('email.allow_idns'));
+        
+        // dot syntax access after property access
+        $this->assertEquals(true, $store->form->get('register.email.allow_idns'));
+        
+        // dot syntax access after array & property access
+        $this->assertEquals(2, $store->form['register']->get('username.min_length'));
+    }
 }
