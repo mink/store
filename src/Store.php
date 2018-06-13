9d2d5794280d71cc2889d4724689bc7d82ddf610
Zac Olden <zacolden@gmail.com>
2018-06-14 00:11:40 +1000

    Return type declarations added

--- src/Store.php
+++ src/Store.php
@@ -21,13 +21,13 @@ class Store extends Collection
         }
     }
 
-    private function createItem($key, $value = [])
+    private function createItem($key, $value = []): void
     {
         $this->items[$key] = (is_array($value) || is_object($value)) ? $this->createNestedItem($value)
             : $this->createSimpleItem($value);
     }
     
-    private function createNestedItem($items = [])
+    private function createNestedItem($items = []): self
     {
         return new self($items, $this);
     }
@@ -56,17 +56,17 @@ class Store extends Collection
         return $this->items[$key];
     }
     
-    public function offsetSet($key, $value)
+    public function offsetSet($key, $value): void
     {
         !$key ? $this->items[] = $value : $this->createItem($key, $value);
     }
     
-    public function offsetExists($key)
+    public function offsetExists($key): bool
     {
         return !empty(array_get($this->items, $key));
     }
     
-    public function offsetUnset($key)
+    public function offsetUnset($key): void
     {
         unset($this->items[$key]);
     }
@@ -82,17 +82,17 @@ class Store extends Collection
         return array_get($this->items, $key);
     }
     
-    public function __set($key, $value)
+    public function __set($key, $value): void
     {
         $this->createItem($key, $value);
     }
     
-    public function __isset($key)
+    public function __isset($key): bool
     {
         return isset($this->items[$key]);
     }
     
-    public function __unset($key)
+    public function __unset($key): void
     {
         if(isset($this->items[$key]))
         {
