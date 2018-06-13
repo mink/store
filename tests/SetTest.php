8b7fdabe01dcddc79b3b06dd917b12d6d9234b7a
Zac Olden <zacolden@gmail.com>
2018-06-13 17:46:43 +1000

    Set tests added

---
+++ tests/SetTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Store\Store;
+use Illuminate\Support\Collection;
+
+final class SetTest extends TestCase
+{
+    public function testIfEmpty()
+    {
+        $store = new Store(['key' => 'value', 'empty_array' => []]);
+        $emptyStore = new Store();
+        
+        // store contains data
+        $this->assertNotEmpty($store);
+        
+        // emptyStore does not contain data
+        $this->assertEmpty($emptyStore);
+        
+        // the key 'key' contains the string 'value'
+        $this->assertNotEmpty($store->get('key'));
+        
+        // the key 'empty_array' is empty
+        $this->assertEmpty($store['empty_array']);
+    }
+    
+    public function testIfNull()
+    {
+        $store = new Store(['key' => 'value', 'empty_array' => []]);
+        $emptyStore = new Store();
+        
+        // a store with data is of course not null
+        $this->assertNotNull($store);
+        
+        // an empty store is not null, it is an instance of Store
+        $this->assertNotNull($emptyStore);
+        
+        // the key empty_array is not considered null, it is transformed
+        // into an instance of Store automatically
+        $this->assertNotNull($store->empty_array);
+    }
+    
+    public function testIfStore()
+    {
+        $store = new Store(['key' => 'value', 'empty_array' => []]);
+        
+        // both are a Store instance, regardless of contained data
+        $this->assertInstanceOf(Store::class, $store);
+        $this->assertInstanceOf(Store::class, $store->empty_array);
+        
+        // the key being searched for will be created automatically
+        // and instanced as a Store if it does not exist
+        $this->assertInstanceOf(Store::class, $store->aaaaaaaa);
+        $this->assertInstanceOf(Store::class, $store['testing']->{123}['hello']);
+        
+        // this key cannot be a Store, it returns a string
+        $this->assertNotInstanceOf(Store::class, $store['key']);
+        
+        // Store extends the Collection of illuminate/support
+        $this->assertInstanceOf(Collection::class, $store);
+        $this->assertInstanceOf(Collection::class, $store->empty_array);
+        $this->assertInstanceOf(Collection::class, $store['doesnt_exist_yet']);
+    }
+}
