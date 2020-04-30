c3832af55db19bd8f6c7a13ba4b9c3d06d753bb7
Zac Olden <zacolden@gmail.com>
2020-05-01 00:52:19 +1000

    Add documentation to JsonStoreCast

--- src/Casts/JsonStoreCast.php
+++ src/Casts/JsonStoreCast.php
@@ -8,13 +8,33 @@ use X\Store\StoreCollection;
 
 class JsonStoreCast implements CastsAttributes
 {
+    /**
+     * Transform the JSON string into a StoreCollection.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     * @return StoreCollection
+     */
     public function get($model, $key, $value, $attributes): StoreCollection
     {
         return new StoreCollection(json_decode($value, true));
     }
 
+    /**
+     * Transform the attribute into a JSON string.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
+     * @return string
+     */
     public function set($model, $key, $value, $attributes): string
     {
-        return ($value instanceof Collection) ? $value->toJson() : json_encode($value);
+        return ($value instanceof Collection)
+            ? $value->toJson()
+            : json_encode($value);
     }
 }
