45389d98516ed8d66bcaa02a414029ab7c83b816
Zac Olden <zacolden@gmail.com>
2020-05-01 00:52:04 +1000

    Add return types to JsonStoreCast

--- src/Casts/JsonStoreCast.php
+++ src/Casts/JsonStoreCast.php
@@ -8,12 +8,12 @@ use X\Store\StoreCollection;
 
 class JsonStoreCast implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes)
+    public function get($model, $key, $value, $attributes): StoreCollection
     {
         return new StoreCollection(json_decode($value, true));
     }
 
-    public function set($model, $key, $value, $attributes)
+    public function set($model, $key, $value, $attributes): string
     {
         return ($value instanceof Collection) ? $value->toJson() : json_encode($value);
     }
